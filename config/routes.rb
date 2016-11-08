84b98c193bbd051bdc3799c8c63d5fb58f0ed6cd
Brennan Butler <bbutler522@gmail.com>
2016-11-08 18:17:15 -0500

    Routes update for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }  
-	resources :posts
+  	devise_for :users, :controllers => { registrations: 'registrations' }   
+	resources :posts do  
+	  resources :comments
+	end  
 
 	root 'posts#index'
 
