7216a8abbcef2e322e67ff646d50805da1adb6cb
Brennan Butler <bbutler522@gmail.com>
2016-11-08 15:54:02 -0500

    Added username to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 before_action :set_post, only: [:show, :edit, :update, :destroy]
 before_action :authenticate_user! 
+before_action :owned_post, only: [:edit, :update, :destroy]  
 
   def index
     @posts = Post.all
@@ -10,15 +11,17 @@ before_action :authenticate_user!
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
-      flash.now[:alert] = "Your new post couldn't be created!  Please check the form."
+      flash[:alert] = "Your new post couldn't be created!  Please check the form."
       render :new
     end
   end
@@ -50,4 +53,11 @@ before_action :authenticate_user!
   def set_post
     @post = Post.find(params[:id])
   end
+
+  def owned_post  
+    unless current_user == @post.user
+      flash[:alert] = "That post doesn't belong to you!"
+      redirect_to root_path
+    end
+  end 
 end
