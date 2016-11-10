b30eb13488e3e5e7d8f00e6619c438e3fc202da4
Brennan Butler <bbutler522@gmail.com>
2016-11-10 14:20:00 -0500

    Fixed image preview when editing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,11 @@ module ApplicationHelper
     image_tag 'default-avatar.jpg', id: 'image-preview',
                                     class: 'img-responsive img-circle profile-image'
   end
+
+  def form_image_select(post)  
+    return image_tag post.image.url(:medium),
+                     id: 'image-preview',
+                     class: 'img-responsive' if post.image.exists?
+    image_tag 'placeholder.jpg', id: 'image-preview', class: 'img-responsive'
+  end
 end
